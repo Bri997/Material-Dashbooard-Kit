46e45cab9b5ee35097be4769bec4b23763976cc8
Bri997 <brian.musial@gmail.com>
2021-01-18 18:11:50 -0600

    added table data to VehicleList

--- src/views/VehicleList/VehicleList.js
+++ src/views/VehicleList/VehicleList.js
@@ -47,7 +47,7 @@ export default function VehicleList() {
     <GridContainer>
       <GridItem xs={12} sm={12} md={12}>
         <Card>
-          <CardHeader color="primary">
+          <CardHeader color='primary'>
             <h4 className={classes.cardTitleWhite}>Simple Table</h4>
             <p className={classes.cardCategoryWhite}>
               Here is a subtitle for this table
@@ -55,53 +55,13 @@ export default function VehicleList() {
           </CardHeader>
           <CardBody>
             <Table
-              tableHeaderColor="primary"
-              tableHead={['Name', 'Country', 'City', 'Salary']}
+              tableHeaderColor='primary'
+              tableHead={['ID', 'Make', 'Model', 'Miles/Hours']}
               tableData={[
-                ['Dakota Rice', 'Niger', 'Oud-Turnhout', '$36,738'],
-                ['Minerva Hooper', 'Curaçao', 'Sinaai-Waas', '$23,789'],
-                ['Sage Rodriguez', 'Netherlands', 'Baileux', '$56,142'],
-                ['Philip Chaney', 'Korea, South', 'Overland Park', '$38,735'],
-                ['Doris Greene', 'Malawi', 'Feldkirchen in Kärnten', '$63,542'],
-                ['Mason Porter', 'Chile', 'Gloucester', '$78,615'],
-              ]}
-            />
-          </CardBody>
-        </Card>
-      </GridItem>
-      <GridItem xs={12} sm={12} md={12}>
-        <Card plain>
-          <CardHeader plain color="primary">
-            <h4 className={classes.cardTitleWhite}>
-              Table on Plain Background
-            </h4>
-            <p className={classes.cardCategoryWhite}>
-              Here is a subtitle for this table
-            </p>
-          </CardHeader>
-          <CardBody>
-            <Table
-              tableHeaderColor="primary"
-              tableHead={['ID', 'Name', 'Country', 'City', 'Salary']}
-              tableData={[
-                ['1', 'Dakota Rice', '$36,738', 'Niger', 'Oud-Turnhout'],
-                ['2', 'Minerva Hooper', '$23,789', 'Curaçao', 'Sinaai-Waas'],
-                ['3', 'Sage Rodriguez', '$56,142', 'Netherlands', 'Baileux'],
-                [
-                  '4',
-                  'Philip Chaney',
-                  '$38,735',
-                  'Korea, South',
-                  'Overland Park',
-                ],
-                [
-                  '5',
-                  'Doris Greene',
-                  '$63,542',
-                  'Malawi',
-                  'Feldkirchen in Kärnten',
-                ],
-                ['6', 'Mason Porter', '$78,615', 'Chile', 'Gloucester'],
+                ['1', 'Chevy', 'S10', '23,000'],
+                ['2', 'Nissan', 'Rouge', '77,500'],
+                ['3', 'John Deere', 'Combine', '5000 hr'],
+                ['4', 'Porsche', 'GT2 RS', '100'],
               ]}
             />
           </CardBody>
